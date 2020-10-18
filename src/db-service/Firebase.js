d3dee9337643f64ce4d6dd265157b7ab682e5f91
shabrul2451 <shabrul2451@gmail.com>
2020-10-19 02:22:42 +0600

    ui and basic operation done

---
+++ src/db-service/Firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from "firebase";
+
+// Your web app's Firebase configuration
+let firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "m",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
+
+// Initialize Firebase
+let fireDb = firebase.initializeApp(firebaseConfig);
+
+export default fireDb.database().ref()
